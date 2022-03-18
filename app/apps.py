ed5b75320c6ec5c238aaf67a518379fc6a0ee18d
Masande Gontyeleni <Masande@badgerholdings.co.za>
2022-03-18 14:40:02 +0200

    Third commit

--- app/apps.py
+++ app/apps.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-=======
-from django.apps import AppConfig
-
-
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
->>>>>>> a8c3a92c522314a3fd91ccb28064329db91097b3
