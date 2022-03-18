529f5db9de1b46a1ecc1e3011ab89e9b36acb453
Masande Gontyeleni <Masande@badgerholdings.co.za>
2022-03-18 14:57:29 +0200

    Fourth commit

--- CovidTracker/asgi.py
+++ CovidTracker/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for CovidTracker project.
 
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CovidTracker.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for CovidTracker project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CovidTracker.settings')
-
-application = get_asgi_application()
->>>>>>> a8c3a92c522314a3fd91ccb28064329db91097b3
