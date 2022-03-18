8875bc40d2dd05623d8cf0bc26bf5bf38214c067
masande-99 <71064104+masande-99@users.noreply.github.com>
2022-03-18 14:43:21 +0200

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.forms import ModelForm
 from django import forms
 from .models import *
@@ -7,14 +6,4 @@ from .models import *
 class ResultsForm(ModelForm):
     class Meta:
         model = Results
-=======
-from django.forms import ModelForm
-from django import forms
-from .models import *
-
-
-class ResultsForm(ModelForm):
-    class Meta:
-        model = Results
->>>>>>> a8c3a92c522314a3fd91ccb28064329db91097b3
         fields = '__all__'
